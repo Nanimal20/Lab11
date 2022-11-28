64c645db9c0dc32d124a974b12c6960d5fd5f996
Nanimal20 <112648377+Nanimal20@users.noreply.github.com>
2022-11-28 13:23:47 -0500

    Adding Files for Lab 11

---
+++ Node.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <memory>
+
+template<class ItemType>
+class Node
+{
+private:
+	ItemType m_value;
+	std::shared_ptr<Node<ItemType>>  m_next;
+
+public:
+	Node() = delete;
+	Node(ItemType val);
+	Node(ItemType val, std::shared_ptr<Node<ItemType>> nextNode);
+	void setItem(const ItemType& val);
+	void setNext(std::shared_ptr<Node<ItemType>> nextNodePtr);
+	int getItem() const;
+	std::shared_ptr<Node<ItemType>> getNext() const;
+	virtual ~Node();
+	
+};
+
+#include "Node.cpp"
